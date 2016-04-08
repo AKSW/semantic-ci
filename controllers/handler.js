f0d539d8d08f396ba76cdab13ca00c6927034da0
Kurt Junghanns <tboonx@googlemail.com>
2016-04-08 16:30:16 +0200

    Added module mz. Made fs asynchronous

--- controllers/handler.js
+++ controllers/handler.js
@@ -2,7 +2,7 @@
 
 const boom = require('boom'),
   exec = require('child-process-promise').exec,
-  fs = require('fs');
+  fs = require('mz').fs;
 
 module.exports = {
   validate: function(request, reply) {
@@ -16,37 +16,44 @@ module.exports = {
 
 function checksyntax(request) {
   const timestamp = new Date();
-  const filename = 'tempfile_rapper_'+timestamp.getTime()+'.dat';
-  fs.writeFile(filename, request.payload.toString());
-  let contentTypeParam = '-g';
-  switch (request.mime) {
-    case 'application/rdf+xml':
-      contentTypeParam = '-i rdfxml';
-      break;
-    case 'application/ld+json':
-    case 'application/rdf+json':
-      contentTypeParam = '-i json';
-      break;
-    case 'application/trig':
-      contentTypeParam = '-i trig';
-      break;
-    case 'application/turtle':
-      contentTypeParam = '-i turtle';
-      break;
-    case 'application/n-triples':
-      contentTypeParam = '-i ntriples';
-      break;
-    case 'application/n-quads':
-      contentTypeParam = '-i nquads';
-      break;
-    case 'text/n3':
-      //not supported with rapper/raptor
-      break;
+  const filename = 'tempfile_rapper_' + timestamp.getTime() + '.dat';
 
-    default:
-      return (new Promise()).reject('WrongMimeType');
-  }
-  return exec('rapper ' + contentTypeParam + ' -o ntriples temp.dat');
+  return fs.writeFile(filename, request.payload.toString())
+    .then((data) => {
+      let contentTypeParam = '-g';
+      switch (request.mime) {
+        case 'application/rdf+xml':
+          contentTypeParam = '-i rdfxml';
+          break;
+        case 'application/ld+json':
+        case 'application/rdf+json':
+          contentTypeParam = '-i json';
+          break;
+        case 'application/trig':
+          contentTypeParam = '-i trig';
+          break;
+        case 'application/turtle':
+          contentTypeParam = '-i turtle';
+          break;
+        case 'application/n-triples':
+          contentTypeParam = '-i ntriples';
+          break;
+        case 'application/n-quads':
+          contentTypeParam = '-i nquads';
+          break;
+        case 'text/n3':
+          //not supported with rapper/raptor
+          break;
+
+        default:
+          return (new Promise()).reject('WrongMimeType');
+      }
+
+      return exec('rapper ' + contentTypeParam + ' -o ntriples temp.dat');
+    })
+    .catch((error) => {
+      return (new Promise()).reject('FailedWritingFile');
+    });
 }
 
 function checkliterals() {
