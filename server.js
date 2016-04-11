31ee1d6c0c1caaf1e0c7d83aa27e423b592c9065
Roy Meissner <roy-meissner@gmx.net>
2016-04-11 17:35:05 +0200

    Added some test cases

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const hapi = require('hapi'),
 
 const server = new hapi.Server();
 let port = (!co.isEmpty(process.env.APPLICATION_PORT)) ? process.env.APPLICATION_PORT : 3000;
-let host = (!co.isEmpty(process.env.VIRTUAL_HOST)) ? process.env.VIRTUAL_HOST : server.info.host;
 server.connection({
   port: port
 });
+
+let host = (!co.isEmpty(process.env.VIRTUAL_HOST)) ? process.env.VIRTUAL_HOST : server.info.host;
 
 module.exports = server;
 
