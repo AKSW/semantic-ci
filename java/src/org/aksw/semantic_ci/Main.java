2b0518dbef8efb485e32f0572199e55b414153f2
Kurt Junghanns <tboonx@googlemail.com>
2016-04-12 15:48:01 +0200

    Created maven java project

---
+++ java/src/org/aksw/semantic_ci/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.aksw.semantic_ci;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+/**
+ * @author kjunghanns
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
